6db3f063c320f8789694eb4a20bdf60a7b95024c
Usuario <usuario@MacBook-Pro-de-Usuario.local>
2021-12-22 20:11:35 +0100

    add final response test

--- controllers/index.controller.test.ts
+++ controllers/index.controller.test.ts
@@ -28,6 +28,25 @@ describe('indexController', () => {
             await indexController.getSocialInfo(req, res);
             expect(res.status).toHaveBeenCalledWith(200);
         });
+        it('should send response with defined properties and values of every social network', async () => {
+            const finalSocialResp = {"facebook": ["status"], "instagram": ["picture"], "twitter": ["tweet"]};
+            const twitterResp = new Promise(resolve => {
+                resolve({data: [{tweet: "tweet"}]})
+            });
+            const facebookResp = new Promise(resolve => {
+                resolve({data: [{status: "status"}]})
+            });
+            const instagramResp = new Promise((resolve, reject) => {
+                resolve({data: [{picture: "picture"}]})
+            });
+            (axios as unknown as jest.Mock).mockResolvedValueOnce(twitterResp);
+            (axios as unknown as jest.Mock).mockResolvedValueOnce(facebookResp);
+            (axios as unknown as jest.Mock).mockResolvedValueOnce(instagramResp);
+            const req = {}
+            const res = { status: jest.fn().mockReturnThis(), json: jest.fn(), send: jest.fn().mockReturnThis()};
+            await indexController.getSocialInfo(req, res);
+            expect(res.send).toHaveBeenCalledWith(finalSocialResp);
+        });
         it('should return error if some social endpoint fails', async () => {
             const twitterResp = new Promise(resolve => {
                 resolve({data: [{tweet: "tweet"}]})
